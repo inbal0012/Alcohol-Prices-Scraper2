83a5401dc439f11450739104d04d99f34f7d8bfc
Inbals <inbal0012@gmail.com>
2022-07-12 00:24:02 +0300

    improve search and add is_product_page

--- main.py
+++ main.py
@@ -30,12 +30,37 @@ def first_attempt():
     # print(soup.find('span', 'aria-label'=re.compile("שם יצרן")))
 
 
-def search_attempt(name):
-    url = "https://www.the-importer.co.il/Web/?PageType=9&SearchResults=true&searchString=" + name
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, "html.parser")
-    # print(soup)
+def data_from_page(soup):
+    name = soup.find('h1', class_=re.compile("product_title"))
+    print("name: " + name.text)
 
+    price = soup.find('p', class_=re.compile("price"))
+    print("price: " + price.text)
+
+    # volume = soup.find('span', id=re.compile("lblItemVolumePer100ml"))
+    # print("volume: " + volume.text)
+
+    # print(soup.find('div', id=re.compile("quantityAndPurchaseButtonsWrapper")))
+    available = soup.find('button', class_=re.compile("add_to_cart_button"))
+    if not available:
+        print("outOfStock")
+
+    # cont = soup.find_all('section', class_=re.compile("elementor-section-full_width"))
+    # cont[0].
+    # print(cont)
+    #
+    # def h2fromcont(container):
+    #     container.find_all()
+    # h2 = map(h2fromcont, cont)
+    # print(soup.find('span'))
+    # print(soup.find_all('td', id=re.compile("rptFeatureTemplateFieldsBelowPic")))
+    # f = soup.find_all('td', id=re.compile("rptFeatureTemplateFieldsBelowPic"))
+
+    # print(f[1])
+    # print(soup.find('span', 'aria-label'=re.compile("שם יצרן")))
+
+
+def data_from_search_list(soup):
     results = soup.find_all('li', id=re.compile("liCustomCatalogItem"))
     for result in results:
         # print(result)
@@ -54,6 +79,35 @@ def search_attempt(name):
         print("")
 
 
+def search_attempt(name):
+    url = "https://www.the-importer.co.il/Web/?PageType=9&SearchResults=true&searchString=" + name
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    # print(soup)
+
+    if is_product_page(soup, name):
+        print("in page")
+        data_from_page(soup)
+    else:
+        print("search")
+        data_from_search_list(soup)
+
+    # print(soup.find('li', id=re.compile("catalogDataList")).find('div', class_=re.compile("list-item-name-wrapper")).find('h2').text)
+
+
+def is_product_page(soup, name):
+    # r = requests.get("https://www.the-importer.co.il/Web/?PageType=9&SearchResults=true&searchString=" + name)
+    # soup = BeautifulSoup(r.content, "html.parser")
+
+    # url = soup.find("meta", property="og:url")
+    # print(url)
+
+    metaKeywords = soup.find("meta", id="metaKeywords")["content"]
+    if re.search(name, metaKeywords):
+        print("product page")
+        return True
+
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -63,8 +117,9 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     # first_attempt()
+    print("-------------------גורדונס---------------------")
     search_attempt('גורדונס')
-    # print("-------------------kalua---------------------")
+    print("-------------------קלואה---------------------")
     search_attempt('קלואה')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
