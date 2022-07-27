a9255ff0d10e7f73486032524f8d905bef8c77ed
Inbals <inbal0012@gmail.com>
2022-07-28 00:48:31 +0300

    add no data found protection in find_element

--- main.py
+++ main.py
@@ -26,12 +26,12 @@ if __name__ == '__main__':
     # importer.search_attempt("ג'יימסון")
     alcohol123 = Alcohol123()
     # alcohol123.first_attempt()
-    items = alcohol123.search_attempt("ג'ק דניאלס")
+    items = alcohol123.search_attempt("פינלנדיה")
     # item = alcohol123.specific_page("https://alcohol123.co.il/product/%d7%95%d7%95%d7%99%d7%a1%d7%a7%d7%99-%d7%92%d7%9c%d7%a0%d7%9c%d7%99%d7%95%d7%95%d7%98-12-%d7%a9%d7%a0%d7%94-700-%d7%9e%d7%9c/")
     # print(items)
     drinks4u = Drinks4u()
     # drinks4u.first_attempt()
-    # items = drinks4u.search_attempt("רום")
+    # items = drinks4u.search_attempt("ג'ק דניאלס")
     # item = drinks4u.specific_page("https://www.drinks4u.co.il/index.php?dir=site&page=catalog&op=item&cs=7050")
     # print(item)
     haturki = Haturki()
@@ -48,15 +48,15 @@ if __name__ == '__main__':
     # google_sheet.save_items(items)
     # google_sheet.is_worksheet_exist("Sheet9")
     alcohol123.set_saver(google_sheet, "Alcohol Prices")
-    alcohol123.save_items(items)
+    # alcohol123.save_items(items)
     # alcohol123.save_item(item)
 
 
 def for_nadav():
     search = []
-    for brand in search:
-        drinks4u = Drinks4u()
-        items = drinks4u.search_attempt(brand)
+    # for brand in search:
+        # drinks4u = Drinks4u()
+        # items = drinks4u.search_attempt(brand)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
