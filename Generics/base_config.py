227d7f331353a431847c9ac712dad731871129c1
Inbals <inbal0012@gmail.com>
2022-07-31 09:28:14 +0300

    add base_config file

---
+++ Generics/base_config.py
@@ -0,0 +1,32 @@
+base_url = 'TODO'
+page = {
+    "name":     {"element": "TODO", "attrs_prop": "class", "attrs": "TODO", "data": "text"},
+    "price":    {"element": "TODO", "attrs_prop": "class", "attrs": "TODO", "data": {
+                 "element": "TODO", "data": "price"}},
+    "volume":   {"element": "TODO", "attrs_prop": "class", "attrs": "TODO", "data": {
+                 "element": "TODO", "data": "text"}},
+    "available": {"element": "TODO", "attrs_prop": "class", "attrs": "TODO", "data": "exist"}
+}
+search = {
+    "name":     {"element": "TODO", "attrs_prop": "class", "attrs": "TODO", "data": {
+                 "element": "TODO", "data": "text"}},
+    "price":    {"element": "TODO", "attrs_prop": "class", "attrs": "TODO", "data": {
+                 "element": "TODO", "attrs_prop": "class", "attrs": "TODO", "data": {
+                 "element": "TODO", "data": "price"}}},
+    "volume":   {"element": "TODO", "attrs_prop": "class", "attrs": "TODO", "data": "text"},
+    "available": {"element": "TODO", "attrs_prop": "class", "attrs": "TODO", "search_word": "TODO", "data": "exist"}
+}
+results = {
+    "element": "TODO",
+    "attrs_prop": "class",
+    "attrs": "TODO"
+}
+product_page_check = {
+    "element": "TODO",
+    "attrs_prop": "class",
+    "attrs": "TODO",
+    "search_word": "TODO",
+    "data": "text"
+}
+search_string = "TODO"
+sheet_name = "Alcohol Prices"
