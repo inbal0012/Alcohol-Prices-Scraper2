c1b85675ce2c7ea0e93397e2f7fec743e5229e6f
Inbals <inbal0012@gmail.com>
2022-08-03 20:33:55 +0300

    redo TheImporter with base_site. search done!

--- Suppliers/the_importer.py
+++ Suppliers/the_importer.py
@@ -1,76 +1,59 @@
-import requests
+from Generics.base_site import BaseSite
 import re
-from bs4 import BeautifulSoup
 
 
-class TheImporter:
-    def data_from_page(self, soup):
-        name = soup.find('h1', id=re.compile("headingText"))
-        print("name: " + name.text)
+class TheImporter(BaseSite):
+    base_url = 'https://www.the-importer.co.il/'
+    page = {
+        "name":     {"element": "TODO", "attrs_prop": "class", "attrs": "TODO", "data": "text"},
+        "price":    {"element": "TODO", "attrs_prop": "class", "attrs": "TODO", "data": {
+                     "element": "TODO", "data": "price"}},
+        "volume":   {"element": "TODO", "attrs_prop": "class", "attrs": "TODO", "data": {
+                     "element": "TODO", "data": "text"}},
+        "available": {"element": "TODO", "attrs_prop": "class", "attrs": "TODO", "data": "exist"}
+    }
+    search = {
+        "name":     {"element": "div", "attrs_prop": "class", "attrs": "list-item-name-wrapper", "data": {
+                     "element": "h2", "data": "text"}},
+        "price":    {"element": "span", "attrs_prop": "class", "attrs": "item-price-value", "data": "text"},
+        "volume":   {"element": "div", "attrs_prop": "class", "attrs": "list-item-name-wrapper", "data": {
+                     "element": "a", "data": "element"}},
+        "available": {"element": "div", "attrs_prop": "class", "attrs": "list-item-pic-wrapper", "search_word": "אזל מהמלאי", "data": {
+                     "element": "img", "attrs_prop": "alt", "attrs": "אזל מהמלאי", "data": "not_exist"}}
+    }
+    results = {
+        "element": "li",
+        "attrs_prop": "id",
+        "attrs": "liCustomCatalogItem"
+    }
+    product_page_check = {
+        "element": "meta",
+        "attrs_prop": "id",
+        "attrs": "metaKeywords",
+        "search_word": "TODO",
+        "data": "text"
+    }
+    search_string = "Web/?PageType=9&SearchResults=true&searchString="
+    sheet_name = "Alcohol Prices"
 
-        price = soup.find('span', id=re.compile("lblTotalPriceStr"))
-        print("price: " + price.text)
-
-        volume = soup.find('span', id=re.compile("lblItemVolumePer100ml"))
-        print("volume: " + volume.text)
-
-        # print(soup.find('div', id=re.compile("quantityAndPurchaseButtonsWrapper")))
-        available = soup.find('div', id=re.compile("quantityAndPurchaseButtonsWrapper")).find("div", id=re.compile(
-            "addToCartBtn"))
-
-        if not available:
-            print("outOfStock")
-
-        # print(soup.find('span'))
-        # print(soup.find_all('td', id=re.compile("rptFeatureTemplateFieldsBelowPic")))
-        f = soup.find_all('td', id=re.compile("rptFeatureTemplateFieldsBelowPic"))
-
-        # print(f[1])
-        # print(soup.find('span', 'aria-label'=re.compile("שם יצרן")))
-
-    def data_from_search_list(self, soup):
-        results = soup.find_all('li', id=re.compile("liCustomCatalogItem"))
-        for result in results:
-            # print(result)
-            name = result.find('div', class_=re.compile("list-item-name-wrapper")).find('h2')
-            # print(name)
-            print(name['aria-label'])
-            # print("Name: " + name.text)
-
-            prize = result.find('div', class_=re.compile("list-item-price-wrapper")).find('span', class_=re.compile(
-                "item-price-value"))
-            print("Prize: " + prize.text)
-
-            outOfStock = result.find('div', class_=re.compile("list-item-pic-wrapper")).find('img', alt=re.compile(
-                "אזל מהמלאי"))
-            if (outOfStock):
-                print("outOfStock")
-
-            print("")
-
-    def search_attempt(self, name):
-        url = "https://www.the-importer.co.il/Web/?PageType=9&SearchResults=true&searchString=" + name
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content, "html.parser")
-        # print(soup)
-
-        if self.is_product_page(soup, name):
-            print("in page")
-            self.data_from_page(soup)
-        else:
-            print("search")
-            self.data_from_search_list(soup)
-
-        # print(soup.find('li', id=re.compile("catalogDataList")).find('div', class_=re.compile("list-item-name-wrapper")).find('h2').text)
+    def __init__(self):
+        """Constructor for testSite"""
+        super().__init__(self.base_url, self.page, self.search, self.results, self.product_page_check, self.search_string)
+        super().create_saver(self.sheet_name)
 
     def is_product_page(self, soup, name):
-        # r = requests.get("https://www.the-importer.co.il/Web/?PageType=9&SearchResults=true&searchString=" + name)
-        # soup = BeautifulSoup(r.content, "html.parser")
-
-        # url = soup.find("meta", property="og:url")
-        # print(url)
-
-        metaKeywords = soup.find("meta", id="metaKeywords")["content"]
-        if re.search(name, metaKeywords):
+        meta_keywords = soup.find("meta", id="metaKeywords")["content"]
+        if re.search(name, meta_keywords):
             print("product page")
             return True
+
+    def search_get_volume(self, soup, dictionary):
+        vol = super().search_get_volume(soup, dictionary)
+        par = vol.find_parent("span")
+        # bottle_volume = soup.find(string="נפח בקבוק").find_parent("div", class_=re.compile("elementor-column"))
+        volume = par.next_sibling.next_sibling.next_sibling.text.split()
+        if "ליטר" in volume:
+                return int(volume[0]) * 1000
+        # todo handle litter
+        print(volume[0])
+        return volume[0]
