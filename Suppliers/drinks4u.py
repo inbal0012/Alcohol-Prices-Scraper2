2788f64761208d9596c7a4a651407effa325b2cb
Inbals <inbal0012@gmail.com>
2022-08-21 11:09:34 +0300

    drinks4u cleanup

--- Suppliers/drinks4u.py
+++ Suppliers/drinks4u.py
@@ -5,18 +5,6 @@ import re
 class Drinks4u(BaseSite):
     base_url = "https://www.drinks4u.co.il/"
     page = {
-        "name": {"element": "h1", "attrs_prop": "class", "attrs": "catalog-title"},
-        "price": {"element": "div", "attrs_prop": "class", "attrs": "price",
-                  "inner_search": {
-                      "element": "span",
-                  }},
-        "volume": {"element": "div", "attrs_prop": "class", "attrs": "prod-summary",
-                   "inner_search": {
-                       "element": "li",
-                   }},
-        "available": {"element": "div", "attrs_prop": "class", "attrs": "product-box-button-quantity", }
-    }
-    page2 = {
         "name": {"element": "h1", "attrs_prop": "class", "attrs": "catalog-title", "data": "text"},
         "price": {"element": "div", "attrs_prop": "class", "attrs": "price", "data": {
                       "element": "span", "data": "price"}},
@@ -25,12 +13,6 @@ class Drinks4u(BaseSite):
         "available": {"element": "div", "attrs_prop": "class", "attrs": "product-box-button-quantity", "data": "exist"}
     }
     search = {
-        "name":     {"element": "div", "attrs_prop": "class", "attrs": "prod-box__title"},
-        "price":    {"element": "div", "attrs_prop": "class", "attrs": "prod-box__price"},
-        "volume":   {"element": "div", "attrs_prop": "class", "attrs": "prod-box__volume"},
-        "available": {"element": "h1",  "attrs_prop": "class", "attrs": "catalog-title", "search_word": "מלאי"}
-    }
-    search2 = {
         "name": {"element": "div", "attrs_prop": "class", "attrs": "prod-box__title", "data": "text"},
         "price": {"element": "div", "attrs_prop": "class", "attrs": "prod-box__price", "data": "price"},
         "volume_per_100": {"element": "div", "attrs_prop": "class", "attrs": "prod-box__volume", "data": "text"},
@@ -58,7 +40,7 @@ class Drinks4u(BaseSite):
 
     def __init__(self):
         """Constructor for testSite"""
-        super().__init__(self.base_url, self.page2, self.search2, self.results, self.product_page_check, self.search_string)
+        super().__init__(self.base_url, self.page, self.search, self.results, self.product_page_check, self.search_string)
         super().create_saver(self.sheet_name)
         self.saver.get_name_index_supplier_col('E')
 
